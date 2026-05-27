ad24161a0174d2a278b0d22c2832947d40db6f9f
Hansika jain <jainhansika62@gmail.com>
2026-05-27 21:29:05 +0530

    Day 27:Merge two sorted list

---
+++ Linked list/Merge_two_sorted_list.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+
+        nums = []
+
+        while list1:
+            nums.append(list1.val)
+            list1 = list1.next
+
+        while list2:
+            nums.append(list2.val)
+            list2 = list2.next
+
+        nums.sort()
+
+        dummy = ListNode(0)
+        curr = dummy
+
+        for num in nums:
+            curr.next = ListNode(num)
+            curr = curr.next
+
+        return dummy.next
